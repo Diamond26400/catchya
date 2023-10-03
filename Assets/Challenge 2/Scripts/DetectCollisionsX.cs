e90d6b0f6e008ef8690d36e90dcd1e2d281bd231
Diamond26400 <obaloluwajames5@gmail.com>
2023-10-03 15:22:26 +0100

    update

--- Assets/Challenge 2/Scripts/DetectCollisionsX.cs
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -1,18 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DetectCollisionsX : MonoBehaviour
-{
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Destroy(gameObject);
-        
-    }
-     void Update()
-    {
-        Debug.Log("Game over");
-    }
-    
-}
+﻿
